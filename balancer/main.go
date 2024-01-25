c9700cb9caf9b63368c981365127e07e53099ce2
MiniJ147 <47308554+MiniJ147@users.noreply.github.com>
2024-01-24 22:45:50 -0500

    created basic reverse porxy

--- balancer/main.go
+++ balancer/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 )
 
 func main() {
@@ -10,8 +13,20 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Recieved connection")
-		w.WriteHeader(200)
+
+		forwardRequest(w, r)
 	})
 
 	http.ListenAndServe(":8000", nil)
+}
+
+func forwardRequest(w http.ResponseWriter, r *http.Request) {
+	url, err := url.Parse("http://localhost:3000") //creating serve url
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//making our proxy
+	reverseProxy := httputil.NewSingleHostReverseProxy(url)
+	reverseProxy.ServeHTTP(w, r)
 }
